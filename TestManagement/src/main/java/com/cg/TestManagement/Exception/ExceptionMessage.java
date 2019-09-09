97583bd5320b038ad70aac13f54d95840ce9d16b
CapgProject <54829831+CapgProject@users.noreply.github.com>
2019-09-09 20:36:52 +0530

    Add files via upload

---
+++ TestManagement/src/main/java/com/cg/TestManagement/Exception/ExceptionMessage.java
@@ -0,0 +1,8 @@
+package com.cg.TestManagement.Exception;
+
+public class ExceptionMessage {
+
+	public static final String IDMESSAGE = "Id cannot be negative or null";
+	public static final String NAMEMESSAGE = "First Character should be capitalized";
+	public static final String PASSWORDMESSAGE = "Password should contain at least one upper case character, one lower case character, one numeric character and one special character";
+}
