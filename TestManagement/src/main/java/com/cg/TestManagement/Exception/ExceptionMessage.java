64a30f2a3d7914f4fc4dc29f1f8cc5a79e29e0e8
PiyushDaswani <piyushdaswani@gmail.com>
2019-09-11 09:17:08 +0530

    modified messages

--- TestManagement/src/main/java/com/cg/TestManagement/Exception/ExceptionMessage.java
+++ TestManagement/src/main/java/com/cg/TestManagement/Exception/ExceptionMessage.java
@@ -4,12 +4,12 @@ public class ExceptionMessage {
 
 	public static final String IDMESSAGE = "Id cannot be negative or null";
 	public static final String NAMEMESSAGE = "First Character should be capitalized";
-	public static final String PASSWORDMESSAGE = "Password should contain at least one upper case character, one lower case character, one numeric character, one special character and only eight characters";
+	public static final String PASSWORDMESSAGE = "Password should contain at least one upper case character, one lower case character, one numeric character, one special character and length should be at least eight characters";
 	public static final String TIMEMESSAGE = "End date cannot be before start date";
 	public static final String ENDTIMEMESSAGE = "End date cannot be in the past";
 	public static final String DURATIONMESSAGE = "The test duration cannot be more than the time between the start and end time";
-	public static final String QUESTIONMESSAGE = "The question does'nt exist in the Test";
-	public static final String USERMESSAGE = "The user does'nt exist";
-	public static final String TESTMESSAGE = "The test does'nt exist";
+	public static final String QUESTIONMESSAGE = "The question does not exist in the Test";
+	public static final String USERMESSAGE = "The user does not exist";
+	public static final String TESTMESSAGE = "The test does not exist";
 	
 }
